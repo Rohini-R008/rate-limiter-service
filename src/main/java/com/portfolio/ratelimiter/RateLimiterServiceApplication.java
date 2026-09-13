0e1f14a963133d1592f7746917ababdca40815c2
Rohini-R008 <rohinir0809@gmail.com>
2026-09-13 14:56:43 +0530

    Added Spring Boot application foundation

---
+++ src/main/java/com/portfolio/ratelimiter/RateLimiterServiceApplication.java
@@ -0,0 +1,11 @@
+package com.portfolio.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimiterServiceApplication.class, args);
+    }
+}
